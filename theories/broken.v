74586b2232dc9f44c38b601089558d7563767231
Jan-Oliver Kaiser <janno@mpi-sws.org>
2020-03-03 23:23:21 +0100

    Add backtrace output.

--- theories/broken.v
+++ theories/broken.v
@@ -12,11 +12,28 @@ Fail Definition broken_backtrace :=
           \nu x,
             let Fx := _ in
             t <- M.abs_fun x Fx; _
-        .
+.
+
+(*
+Cannot infer the type of x in
+environment:
+go : forall t : unit, M {t' : unit & t' = t}
+t : unit
+Raised at file "hashtbl.ml", line 506, characters 17-32
+Called from file "kernel/cClosure.ml", line 522, characters 4-25
+ *)
 
 Fail Definition working_backtrace :=
       fix go (t : unit) {struct t} : M {t' & t' = t} :=
           \nu x,
             let Fx := _ in
             t <- M.abs_fun x _; _
-        .
+.
+(*
+Cannot infer the type of x in
+environment:
+go : forall t : unit, M {t' : unit & t' = t}
+t : unit
+Raised at file "map.ml", line 135, characters 10-25
+Called from file "pretyping/globEnv.ml", line 59, characters 6-32
+ *)
